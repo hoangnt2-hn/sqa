b0d257671f7a0071bd4ce56e5ef204a98d54219a
hoangnt.working <hoangnt.working@gmail.com>
2019-03-12 23:05:56 +0700

    update

---
+++ sqa/src/main/java/com/hoangnt/model/ResetPasswordDTO.java
@@ -0,0 +1,23 @@
+package com.hoangnt.model;
+
+public class ResetPasswordDTO {
+	String old_pass;
+	String new_pass;
+
+	public String getOld_pass() {
+		return old_pass;
+	}
+
+	public void setOld_pass(String old_pass) {
+		this.old_pass = old_pass;
+	}
+
+	public String getNew_pass() {
+		return new_pass;
+	}
+
+	public void setNew_pass(String new_pass) {
+		this.new_pass = new_pass;
+	}
+
+}
