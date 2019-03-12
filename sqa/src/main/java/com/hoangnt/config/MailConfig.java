cb77037effc6dd5ba56e687761beefef2fc687d4
hoangnt.working <hoangnt.working@gmail.com>
2019-03-12 13:58:49 +0700

    update

---
+++ sqa/src/main/java/com/hoangnt/config/MailConfig.java
@@ -0,0 +1,32 @@
+package com.hoangnt.config;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import com.hoangnt.ultis.MyConstants;
+
+@Configuration
+public class MailConfig {
+	@Bean
+	public JavaMailSender getJavaMailSender() {
+		JavaMailSenderImpl mailSenderImpl = new JavaMailSenderImpl();
+		mailSenderImpl.setHost("smtp.gmail.com");
+		mailSenderImpl.setPort(587);
+
+		mailSenderImpl.setUsername(MyConstants.MY_EMAIL);
+		mailSenderImpl.setPassword(MyConstants.MY_PASSWORD);
+
+		Properties properties = mailSenderImpl.getJavaMailProperties();
+		properties.put("mail.transport.protocol", "smtp");
+		properties.put("mail.smtp.auth", "true");
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.debug", "true");
+
+		return mailSenderImpl;
+	}
+
+}
---
+++ sqa/src/main/java/com/hoangnt/ultis/MyConstants.java
@@ -0,0 +1,7 @@
+package com.hoangnt.ultis;
+
+public class MyConstants {
+	public static final String MY_EMAIL = "torihoang1@gmail.com";
+
+	public static final String MY_PASSWORD = "hoanghoitham";
+}
